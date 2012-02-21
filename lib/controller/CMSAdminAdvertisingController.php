ce7d3ab8f66b528c1b16d9709f8cf8691398e5e7
Sheldon <sheldon.els@gmail.com>
2012-02-21 15:31:41 +0000

    initial controller + model. basic ad fields with attached media.

---
+++ lib/controller/CMSAdminAdvertisingController.php
@@ -0,0 +1,10 @@
+<?
+class CMSAdminAdvertisingController extends AdminComponent{
+
+  public $module_name = "advertising";
+  public $model_class = 'WildfireAdvertAdvanced';
+  public $display_name = "Advertising";
+  public $dashboard = false;
+  public $file_tags = array('media');
+
+}?>
---
+++ resources/app/controller/admin/AdminAdvertisingController.php
@@ -0,0 +1,5 @@
+<?
+class AdminAdvertisingController extends CMSAdminAdvertisingController{
+  
+}
+?>

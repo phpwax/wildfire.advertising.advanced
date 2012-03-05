5dbdcb876a1cca151b2bd1b7e20825f1182392b7
charlesmarshall <cm56marshall@googlemail.com>
2012-03-05 17:42:11 +0000

    make that static so it can be changed per site

--- lib/model/WildfireAdvertAdvanced.php
+++ lib/model/WildfireAdvertAdvanced.php
@@ -1,12 +1,12 @@
 <?
 class WildfireAdvertAdvanced extends WaxModel{
-  public $ad_types = array(''=>'-- Select --', 'mpu'=>'MPU', 'leaderboard'=>'Leaderboard', 'sky'=>'Sky');
+  public static $ad_types = array(''=>'-- Select --', 'mpu'=>'MPU', 'leaderboard'=>'Leaderboard', 'sky'=>'Sky');
 
   public function setup(){
     if($types = Config::get("adverts/types")) $this->ad_types = $types;
 
     $this->define("title", "CharField", array('required'=>true,'scaffold'=>true));
-    $this->define("type", "CharField", array('widget'=>'SelectInput', 'choices'=>$this->ad_types,'scaffold'=>true));
+    $this->define("type", "CharField", array('widget'=>'SelectInput', 'choices'=>WildfireAdvertAdvanced::$ad_types,'scaffold'=>true));
     $this->define("link", "CharField", array('required'=>true));
     $this->define("impressions", "IntegerField", array('editable'=>false,'scaffold'=>true));
     $this->define("clicks", "IntegerField", array('editable'=>false,'scaffold'=>true));
@@ -36,6 +36,6 @@ class WildfireAdvertAdvanced extends WaxModel{
   }
 
   public function groups(){
-    return $this->ad_types[$this->type];
+    return WildfireAdvertAdvanced::$ad_types[$this->type];
   }
 }?>

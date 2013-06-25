9342699fb5acad7043f5ef64586ccaa5d4687bba
Sheldon Els <sheldon.els@gmail.com>
2013-06-25 13:52:39 +0100

    composer basics

--- setup.php
+++ setup.php
@@ -1,5 +1,11 @@
 <?
+print_r("here");exit;
   CMSApplication::register_module("advertising", array('plugin_name'=>'wildfire.advertising.advanced', 'assets_for_cms'=>true, "display_name"=>"Advertising", "link"=>"/admin/advertising/", 'split'=>true));
+
+  AutoLoader::register_view_path("plugin", __DIR__."/view/");
+  AutoLoader::register_controller_path("plugin", __DIR__."/lib/controller/");
+  AutoLoader::register_controller_path("plugin", __DIR__."/resources/app/controller/");
+  AutoLoader::$plugin_array[] = array("name"=>"wildfire.advertising.advanced","dir"=>__DIR__);
 
   if(!defined("CONTENT_MODEL")){
     $con = new ApplicationController(false, false);

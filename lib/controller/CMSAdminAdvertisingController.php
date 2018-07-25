fe6206099c0eeeb8a6a2439c5cebbe5b3f852f97
Michal Noskovic <michal@oneblackbear.com>
2018-07-25 16:46:47 +0100

    don’t upload nor allow for preview hover also fix filters

--- lib/controller/CMSAdminAdvertisingController.php
+++ lib/controller/CMSAdminAdvertisingController.php
@@ -6,4 +6,9 @@ class CMSAdminAdvertisingController extends AdminMediaController{
   public $display_name = "Advertising";
   public $dashboard = false;
   public $file_tags = array('media');
+  public $uploads = false;
+  public $preview_hover = false;
+  public $filter_fields=array(
+                          'text' => array('columns'=>array('title', 'type'), 'partial'=>'_filters_text', 'fuzzy'=>true)
+                        );
 }?>
